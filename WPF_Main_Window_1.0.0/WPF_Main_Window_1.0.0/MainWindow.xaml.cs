8e0486e0ab45e2e578a5dfea2aa6f2463f47077d
CodingSchoolDog <89632518+CodingSchoolDog@users.noreply.github.com>
2021-09-01 10:49:45 +0900

    wpf 최초 테스트
    wpf 최초 테스트

---
+++ WPF_Main_Window_1.0.0/WPF_Main_Window_1.0.0/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Main_Window_1._0._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            var a = new TextBox();
+            a.Height = 40;
+            a.Width = 40;
+            Grid grid = new Grid();
+            grid.Children.Add(a);
+
+            a.Margin
+
+            UserControl u1 = new UserControl();
+            u1.Height = 200;
+            u1.Width = 200;
+            u1.Content = grid;
+            a1.Children.Add(u1);
+            */
+        }
+    }
+}
