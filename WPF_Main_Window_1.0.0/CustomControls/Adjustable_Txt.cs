a6897f84c053e81601f5967c074858e5f771f565
CodingSchoolDog <schooldog07@dgu.ac.kr>
2021-09-01 10:34:34 +0900

    마우스호버, 좌상단 resizing

---
+++ WPF_Main_Window_1.0.0/CustomControls/Adjustable_Txt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Main_Window_1._0._0.CustomControls
+{
+    public class Adjustable_Txt:Control
+    {
+        static Adjustable_Txt()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Adjustable_Txt), new FrameworkPropertyMetadata(typeof(Adjustable_Txt)));
+
+        }
+    }
+}
