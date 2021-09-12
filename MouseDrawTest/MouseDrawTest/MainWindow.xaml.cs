cc5d62654b11e79a478be96f3afbe8373bd54e57
CodingSchoolDog <89632518+CodingSchoolDog@users.noreply.github.com>
2021-09-13 00:52:10 +0900

    마우스 그리기, 손떨림 보정 테스트코드(매우 불안정) +해당 기능 포기 논의 필요

---
+++ MouseDrawTest/MouseDrawTest/MainWindow.xaml.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Linq;
+
+namespace MouseDrawTest
+{
+    
+    public partial class MainWindow : Window
+    {
+        bool drawing = false;
+        List<Point> pointCollection = new List<Point>();
+        List<Line> lineCollection = new List<Line>();
+        Point currentPoint = new Point();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            drawing = true;
+            currentPoint = e.GetPosition(this); pointCollection.Add(currentPoint);
+        }
+
+        private void Canvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+                line.StrokeThickness = 2;
+                line.Opacity = 0.5;
+                line.Stroke = System.Windows.SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+                //line.render
+                currentPoint = e.GetPosition(this);
+                lineCollection.Add(line);
+                pointCollection.Add(currentPoint);
+                paintSurface.Children.Add(line);
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (drawing && lineCollection.Count > 1) 
+            {
+                //DouglasPeuckerReduction(pointCollection, 2)
+                Path path = MakeCurve(DouglasPeuckerReduction(pointCollection, 0.4).ToArray(), 1.2);
+                path.Stroke = Brushes.Red;
+                path.StrokeThickness = 2;
+                path.Opacity = 0.5;
+                paintSurface.Children.Add(path);
+
+            }
+            
+            drawing = false;
+            pointCollection.Clear();
+            lineCollection.Clear();
+        }
+
+
+
+        
+        private Path MakeCurve(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            Point[] result_points = MakeCurvePoints(points, tension);
+
+            // Use the points to create the path.
+            return MakeBezierPath(result_points.ToArray());
+        }
+        private Path MakeBezierPath(Point[] points)
+        {
+            // Create a Path to hold the geometry.
+            Path path = new Path();
+
+            // Add a PathGeometry.
+            PathGeometry path_geometry = new PathGeometry();
+            path.Data = path_geometry;
+
+            // Create a PathFigure.
+            PathFigure path_figure = new PathFigure();
+            path_geometry.Figures.Add(path_figure);
+
+            // Start at the first point.
+            path_figure.StartPoint = points[0];
+
+            // Create a PathSegmentCollection.
+            PathSegmentCollection path_segment_collection =
+                new PathSegmentCollection();
+            path_figure.Segments = path_segment_collection;
+
+            // Add the rest of the points to a PointCollection.
+            PointCollection point_collection =
+                new PointCollection(points.Length - 1);
+            for (int i = 1; i < points.Length; i++)
+                point_collection.Add(points[i]);
+
+            // Make a PolyBezierSegment from the points.
+            PolyBezierSegment bezier_segment = new PolyBezierSegment();
+            bezier_segment.Points = point_collection;
+
+            // Add the PolyBezierSegment to othe segment collection.
+            path_segment_collection.Add(bezier_segment);
+
+            return path;
+        }
+        private Point[] MakeCurvePoints(Point[] points, double tension)
+        {
+            if (points.Length < 2) return null;
+            double control_scale = tension / 0.5 * 0.175;
+
+            // Make a list containing the points and
+            // appropriate control points.
+            List<Point> result_points = new List<Point>();
+            result_points.Add(points[0]);
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                // Get the point and its neighbors.
+                Point pt_before = points[Math.Max(i - 1, 0)];
+                Point pt = points[i];
+                Point pt_after = points[i + 1];
+                Point pt_after2 = points[Math.Min(i + 2, points.Length - 1)];
+
+                double dx1 = pt_after.X - pt_before.X;
+                double dy1 = pt_after.Y - pt_before.Y;
+
+                Point p1 = points[i];
+                Point p4 = pt_after;
+
+                double dx = pt_after.X - pt_before.X;
+                double dy = pt_after.Y - pt_before.Y;
+                Point p2 = new Point(
+                    pt.X + control_scale * dx,
+                    pt.Y + control_scale * dy);
+
+                dx = pt_after2.X - pt.X;
+                dy = pt_after2.Y - pt.Y;
+                Point p3 = new Point(
+                    pt_after.X - control_scale * dx,
+                    pt_after.Y - control_scale * dy);
+
+                // Save points p2, p3, and p4.
+                result_points.Add(p2);
+                result_points.Add(p3);
+                result_points.Add(p4);
+            }
+
+            // Return the points.
+            return result_points.ToArray();
+        }
+
+
+        /// <summary>
+        /// Uses the Douglas Peucker algorithm to reduce the number of points.
+        /// </summary>
+        /// <param name="Points">The points.</param>
+        /// <param name="Tolerance">The tolerance.</param>
+        /// <returns></returns>
+        public static List<Point> DouglasPeuckerReduction
+            (List<Point> Points, Double Tolerance)
+        {
+            if (Points == null || Points.Count < 3)
+                return Points;
+
+            Int32 firstPoint = 0;
+            Int32 lastPoint = Points.Count - 1;
+            List<Int32> pointIndexsToKeep = new List<Int32>();
+
+            //Add the first and last index to the keepers
+            pointIndexsToKeep.Add(firstPoint);
+            pointIndexsToKeep.Add(lastPoint);
+
+            //The first and the last point cannot be the same
+            while (Points[firstPoint].Equals(Points[lastPoint]))
+            {
+                lastPoint--;
+            }
+
+            DouglasPeuckerReduction(Points, firstPoint, lastPoint,
+            Tolerance, ref pointIndexsToKeep);
+
+            List<Point> returnPoints = new List<Point>();
+            pointIndexsToKeep.Sort();
+            foreach (Int32 index in pointIndexsToKeep)
+            {
+                returnPoints.Add(Points[index]);
+            }
+
+            return returnPoints;
+        }
+
+        /// <summary>
+        /// Douglases the peucker reduction.
+        /// </summary>
+        /// <param name="points">The points.</param>
+        /// <param name="firstPoint">The first point.</param>
+        /// <param name="lastPoint">The last point.</param>
+        /// <param name="tolerance">The tolerance.</param>
+        /// <param name="pointIndexsToKeep">The point index to keep.</param>
+        private static void DouglasPeuckerReduction(List<Point>
+            points, Int32 firstPoint, Int32 lastPoint, Double tolerance,
+            ref List<Int32> pointIndexsToKeep)
+        {
+            Double maxDistance = 0;
+            Int32 indexFarthest = 0;
+
+            for (Int32 index = firstPoint; index < lastPoint; index++)
+            {
+                Double distance = PerpendicularDistance
+                    (points[firstPoint], points[lastPoint], points[index]);
+                if (distance > maxDistance)
+                {
+                    maxDistance = distance;
+                    indexFarthest = index;
+                }
+            }
+
+            if (maxDistance > tolerance && indexFarthest != 0)
+            {
+                //Add the largest point that exceeds the tolerance
+                pointIndexsToKeep.Add(indexFarthest);
+
+                DouglasPeuckerReduction(points, firstPoint,
+                indexFarthest, tolerance, ref pointIndexsToKeep);
+                DouglasPeuckerReduction(points, indexFarthest,
+                lastPoint, tolerance, ref pointIndexsToKeep);
+            }
+        }
+
+        /// <summary>
+        /// The distance of a point from a line made from point1 and point2.
+        /// </summary>
+        /// <param name="pt1">The PT1.</param>
+        /// <param name="pt2">The PT2.</param>
+        /// <param name="p">The p.</param>
+        /// <returns></returns>
+        public static Double PerpendicularDistance
+            (Point Point1, Point Point2, Point Point)
+        {
+            //Area = |(1/2)(x1y2 + x2y3 + x3y1 - x2y1 - x3y2 - x1y3)|   *Area of triangle
+            //Base = v((x1-x2)²+(x1-x2)²)                               *Base of Triangle*
+            //Area = .5*Base*H                                          *Solve for height
+            //Height = Area/.5/Base
+
+            Double area = Math.Abs(.5 * (Point1.X * Point2.Y + Point2.X *
+            Point.Y + Point.X * Point1.Y - Point2.X * Point1.Y - Point.X *
+            Point2.Y - Point1.X * Point.Y));
+            Double bottom = Math.Sqrt(Math.Pow(Point1.X - Point2.X, 2) +
+            Math.Pow(Point1.Y - Point2.Y, 2));
+            Double height = area / bottom * 2;
+
+            return height;
+
+            //Another option
+            //Double A = Point.X - Point1.X;
+            //Double B = Point.Y - Point1.Y;
+            //Double C = Point2.X - Point1.X;
+            //Double D = Point2.Y - Point1.Y;
+
+            //Double dot = A * C + B * D;
+            //Double len_sq = C * C + D * D;
+            //Double param = dot / len_sq;
+
+            //Double xx, yy;
+
+            //if (param < 0)
+            //{
+            //    xx = Point1.X;
+            //    yy = Point1.Y;
+            //}
+            //else if (param > 1)
+            //{
+            //    xx = Point2.X;
+            //    yy = Point2.Y;
+            //}
+            //else
+            //{
+            //    xx = Point1.X + param * C;
+            //    yy = Point1.Y + param * D;
+            //}
+
+            //Double d = DistanceBetweenOn2DPlane(Point, new Point(xx, yy));
+        }
+        
+    }
+}
